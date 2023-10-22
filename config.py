3f71ec4341dc1e2cbd855243d982570c58056bbf
MD Rafiqul Islam <110759994+rafiqul-altersense@users.noreply.github.com>
2023-10-23 02:25:06 +0600

    init

---
+++ config.py
@@ -0,0 +1,5 @@
+
+CLIENT_ID = "126271AE72B94DBA87A13A7C46A4CA69"
+CLIENT_SECRET = "h_HwYZAqmv3XkXLg29d3LLuxL8auFiDHkzmEv2Cqhd5tLWit"
+redirect_url = 'https://xero.com/'
+scope = 'offline_access accounting.transactions'
